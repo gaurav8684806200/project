406f0e14742ecaf548931f47c5bc75cf1d300a4d
Gaurav <hr55qt5211@gmail.com>
2026-03-06 23:17:49 +0530

    first commit

---
+++ calculator01.py
@@ -0,0 +1,17 @@
+m = int(input('enter the first number: '))
+n = int(input('enter the second number:'))
+Ans1 = m + n 
+print('the sum of the two numbers is:' , Ans1)
+Ans2 = m - n
+print('the difference of the two numbers is:', Ans2)
+Ans3 = m * n
+print('the product of the two numbers is:', Ans3)
+Ans4 = m / n
+print('the quotient of the two number is:', Ans4)
+Ans5 = m % n
+print('the reminder two numbers is :', Ans5)
+Ans6 = m ** n 
+print('the result of the first number raised to the power of the second number is :', Ans6)
+
+print('THANK YOU FOR USING THIS CALCULATOR')
+print('THIS CALCULATOR WAS CREATED BY GAURAV SHARMA')
