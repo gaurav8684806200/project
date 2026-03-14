938f0d4f1a6d5e882143b0fe520e04f29e1d6089
Gaurav <hr55qt5211@gmail.com>
2026-03-14 21:26:57 +0530

    add exercise2 file

---
+++ exercise2.py
@@ -0,0 +1,10 @@
+# good morning sir ,, time wise goodmorning , good Afternoon , good night project  ,,
+import time
+timestamp = time.strftime("%H:%M:%S")
+print("current time is: " +timestamp)
+timestamp = time.strftime("%H")
+print("current hour is: " +timestamp)
+timestamp = time.strftime("%M")
+print("current minute is: " + timestamp)
+timestamp = time.strftime("%S")
+print("current second is: " + timestamp)
